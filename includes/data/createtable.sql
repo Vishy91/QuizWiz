40b3520c17d36ac3c118ce73a5d5546ff6a9f73d
Shweta Bhartia <shweta.bhartia93@gmail.com>
2018-04-11 19:43:24 -0400

    Added dbconfig and updated create table(auto increment)

--- includes/data/createtable.sql
+++ includes/data/createtable.sql
@@ -1,17 +1,19 @@
+CREATE DATABASE QUIZWIZ;
+
 CREATE TABLE USERS(
-	user_id INT PRIMARY KEY NOT NULL,
+	user_id SERIAL INT PRIMARY KEY NOT NULL,
 	user_firstname TEXT NOT NULL,
 	user_lastname INT NOT NULL,
 );
 
 CREATE TABLE CATEGORIES(
-	category_id INT PRIMARY KEY NOT NULL,
+	category_id SERIAL INT PRIMARY KEY NOT NULL,
 	category_name TEXT NOT NULL
 );
 
 --users and categories table mapping
 CREATE TABLE SUBSCRIPTIONS(
-	subscription_id INT PRIMARY KEY NOT NULL,
+	subscription_id SERIAL INT PRIMARY KEY NOT NULL,
 	subscription_user_id INT,
 	subscription_category_id INT,
 	FOREIGN KEY (subscription_user_id) REFERENCES USERS (user_id),
@@ -19,21 +21,21 @@ CREATE TABLE SUBSCRIPTIONS(
 );
 
 CREATE TABLE TOPICS(
-	topic_id INT PRIMARY KEY NOT NULL,
+	topic_id SERIAL INT PRIMARY KEY NOT NULL,
 	topic_name TEXT NOT NULL,
 	topic_category_id INT,
 	FOREIGN KEY (topic_category_id) REFERENCES CATEGORIES (category_id)
 );
 
 CREATE TABLE QUIZZES(
-	quiz_id INT PRIMARY KEY NOT NULL,
+	quiz_id SERIAL INT PRIMARY KEY NOT NULL,
 	quiz_name TEXT NOT NULL,
 	quiz_topic_id INT,
 	FOREIGN KEY (quiz_topic_id) REFERENCES TOPICS (topic_id)
 );
 
 CREATE TABLE QUESTIONS(
-	question_id INT PRIMARY KEY NOT NULL,
+	question_id SERIAL INT PRIMARY KEY NOT NULL,
 	question_text TEXT NOT NULL,
 	question_quiz_id INT,
 	FOREIGN KEY (question_quiz_id) REFERENCES QUIZZES (quiz_id)
@@ -41,7 +43,7 @@ CREATE TABLE QUESTIONS(
 
 --Quiz played by a user
 CREATE TABLE USERQUIZ(
-	userquiz_id INT PRIMARY KEY NOT NULL,
+	userquiz_id SERIAL INT PRIMARY KEY NOT NULL,
 	userquiz_user_id INT,
 	userquiz_quiz_id INT,
 	FOREIGN KEY (userquiz_user_id) REFERENCES USERS (user_id)
@@ -52,7 +54,7 @@ CREATE TABLE USERQUIZ(
 --user1 : challenger user id
 --user2 : challenged to user id
 CREATE TABLE CHALLENGES(
-	challenge_id INT PRIMARY KEY NOT NULL,
+	challenge_id SERIAL INT PRIMARY KEY NOT NULL,
 	challenge_quiz_id INT,
 	challenge_user1_id INT,
 	challenge_user2_id INT,
