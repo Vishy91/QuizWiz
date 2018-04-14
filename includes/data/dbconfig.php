5b3007f0fcf46ef115ff5fa2fb28ad644723bc41
Shweta Bhartia <shweta.bhartia93@gmail.com>
2018-04-14 17:15:36 -0400

    Db connection, index and sign up page

--- includes/data/dbconfig.php
+++ includes/data/dbconfig.php
@@ -1,16 +1,7 @@
 <?php
-  define("DB_SERVER", "localhost");
-  define("DB_USER", "root");
-  define("DB_PASS", "ShwetaBhartia");
-  define("DB_NAME", "QUIZWIZ");
 
-  // 1. Create a database connection
-  $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-  // Test if connection succeeded
-  if(mysqli_connect_errno()) {
-    die("Database connection failed: " . 
-         mysqli_connect_error() . 
-         " (" . mysqli_connect_errno() . ")"
-    );
-  }
+  $conn_string = "host=localhost port=5432 dbname=QUIZWIZ user=root password=ShwetaBhartia";
+  //$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+  //"host=localhost dbname=DBNAME user=USERNAME password=PASSWORD"
+  $db_connection = pg_connect($conn_string) or die("Database connection failed");
 ?>
