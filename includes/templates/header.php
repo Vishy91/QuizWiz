e6eac43c1d94ae2a59b47f22f26904147f5f5e78
vishy91 <vaishnavi.mukundhan@gmail.com>
2018-04-15 17:07:25 -0400

    home page

---
+++ includes/templates/header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> QuizWiz</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<div class="body-wrapper"  style="font-family: Roboto, sans-serif;font-size: 20px;
+    color: white;
+    font-weight: 500;">
+
+    <!--    <!-- partial -->-->
+    <!-- partial:partials/_navbar.html -->
+    <header class="mdc-toolbar mdc-elevation--z4 mdc-toolbar--fixed" style="background-color: #563d7c;" >
+        <div class="mdc-toolbar__row">
+            <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
+                <div class="logo">
+                    <img style="width:50px; height:50px;" src="../public/images/a.png">
+                </div>
+                <div class="brand"> QuizWiz </div>
+
+                <span class="mdc-toolbar__input">
+            <div class="mdc-text-field">
+              <input type="text" style="background-color: white; color: #563d7c; !important;" class="mdc-text-field__input" id="css-only-text-field-box" placeholder="Search">
+            </div>
+          </span>
+            </section>
+            <section class="mdc-toolbar__section mdc-toolbar__section--align-end" role="toolbar">
+                <div class="mdc-menu-anchor">
+                    <a href="#" class="mdc-toolbar__icon toggle mdc-ripple-surface" data-toggle="dropdown" toggle-dropdown="notification-menu" data-mdc-auto-init="MDCRipple">
+                        <i class="material-icons">notifications</i>
+                        <span class="dropdown-count">3</span>
+                    </a>
+                    <div class="mdc-simple-menu mdc-simple-menu--right" tabindex="-1" id="notification-menu">
+                        <ul class="mdc-simple-menu__items mdc-list" role="menu" aria-hidden="true">
+                            <li class="mdc-list-item" role="menuitem" tabindex="0">
+                                <i class="material-icons mdc-theme--primary mr-1">email</i>
+                                One unread message
+                            </li>
+                            <li class="mdc-list-item" role="menuitem" tabindex="0">
+                                <i class="material-icons mdc-theme--primary mr-1">group</i>
+                                One event coming up
+                            </li>
+                            <li class="mdc-list-item" role="menuitem" tabindex="0">
+                                <i class="material-icons mdc-theme--primary mr-1">cake</i>
+                                It's Aleena's birthday!
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="mdc-menu-anchor">
+                    <a href="#" class="mdc-toolbar__icon mdc-ripple-surface" data-mdc-auto-init="MDCRipple">
+                        <i class="material-icons">widgets</i>
+                    </a>
+                </div>
+                <div class="mdc-menu-anchor mr-1">
+                    <a href="#" class="mdc-toolbar__icon toggle mdc-ripple-surface" data-toggle="dropdown" toggle-dropdown="logout-menu" data-mdc-auto-init="MDCRipple">
+                        <i class="material-icons">more_vert</i>
+                    </a>
+                    <div class="mdc-simple-menu mdc-simple-menu--right" tabindex="-1" id="logout-menu">
+                        <ul class="mdc-simple-menu__items mdc-list" role="menu" aria-hidden="true">
+                            <li class="mdc-list-item" role="menuitem" tabindex="0">
+                                <i class="material-icons mdc-theme--primary mr-1">settings</i>
+                                Settings
+                            </li>
+                            <li class="mdc-list-item" role="menuitem" tabindex="0">
+                                <i class="material-icons mdc-theme--primary mr-1">power_settings_new</i>
+                                Logout
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </header>
+</div>
+</body>
+</html>
--- public/home.php
+++ public/home.php
@@ -6,6 +6,250 @@
  * Time: 6:23 PM
  */
 include("../includes/templates/header.php");
-require_once("../includes/data/dbconfig.php");
-?>
+//require_once("../includes/data/dbconfig.php");
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> QuizWiz</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="node_modules/mdi/css/materialdesignicons.min.css">
+
+    <!-- inject:css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- endinject -->
+<!--    <link rel="shortcut icon" href="images/favicon.png" />-->
+<!--    Includes for the cards-->
+    <link rel="stylesheet" type="text/html" href="css/normalize.css">
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.min.css'>
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css'>
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css'>
+    <link rel="stylesheet" href="css/style.css">
+<!--    End-->
+</head>
+
+<body>
+<div class="body-wrapper">
+
+<!--    <!-- partial -->-->
+
+    <div class="page-wrapper mdc-toolbar-fixed-adjust" style="padding-top: 2%; padding-right: 2%">
+        <main class="content-wrapper" style="padding: 15px 15px 15px 15px !important;">
+<!--            <div class="mdc-layout-grid">-->
+<!--                <div class="mdc-layout-grid__inner">-->
+<!--                    <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12" style="height: 150%;">-->
+                        <div class="mdc-card" style="height: 150%;background-color:#563d7c; padding: 2% 2% 2% 2% !important; ">
+                            <div class="mdc-layout-grid__inner" >
+                                <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-3">
+                                    <section class="purchase__card_section" style="padding: 0px !important;">
+                                        <img src="images/faces/face1.jpg">
+                                    </section>
+                                </div>
+                                <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-3">
+                                    <section class="purchase__card_section">
+                                        <p>Welcome, User.</p>
+                                    </section>
+                                </div>
+                                <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-5">
+                                    <section class="purchase__card_section d-flex align-item-center">
+                                        Game Statistics:<br>
+                                        Games Won: 7
+                                        Lost: 5
+                                        Draw : 15
+                                    </section>
+                                </div>
+                            </div>
+                        </div>
+
+            <br>
+            <br>
+            <div class="row">
+                <div class="col-sm-9 col-md-6 col-lg-8" >
+                    <div class="card" >
+                            <div class="card-header">
+                    <ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link" role="tab" id="cat-tab" data-toggle="tab" href="#categories" aria-controls="cat" aria-selected="false"
+                            style="">Categories</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" role="tab" id="play-tab" data-toggle="tab" href="#played"  aria-controls="play" aria-selected="false">Played</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" role="tab" id="stat-tab" data-toggle="tab" href="#details"  aria-controls="stat" aria-selected="false"> detailedStatistics</a>
+                        </li>
+                    </ul>
+                </div>
+                            <div class="card-body">
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="categories"  role="tabpanel" aria-labelledby="cat-tab">
+                            <div class="container-fluid">
+                                <div class="row" style="padding: 2% 2% 2% 2%">
+                                    <div class="col-sm-3 col-md-3">
+                                        <!-- Card -->
+                                        <article class="card card-inverse animated fadeInLeft">
+                                            <img class="img-responsive" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/1U2EGZ07GU.jpg" alt="Deer in nature" />
+                                            <div class="card-img-overlay">
+                                                <p class="card-text">Computer Science</p>
+                                            </div>
+                                        </article><!-- .end Card -->
+                                    </div>
+                                    <div class="col-sm-3 col-md-3">
+                                        <!-- Card -->
+                                        <article class="card card-inverse animated fadeInDown">
+                                            <img class="img-responsive" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/SYC0YBA37V.jpg" alt="Leaf on the street" />
+                                            <div class="card-img-overlay">
+                                                <p class="card-text">Big Bang Theory</p>
+                                            </div>
+                                        </article><!-- .end Card -->
+                                    </div>
+                                    <div class="col-sm-3 col-md-3">
+                                        <!-- Card -->
+                                        <article class="card card-inverse animated fadeInRight">
+                                            <img class="img-responsive" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/8SALDQRRZX.jpg" alt="White sand" />
+                                            <div class="card-img-overlay">
+                                                <p class="card-text">Bloomington</p>
+                                            </div>
+                                        </article><!-- .end Card -->
+                                    </div>
+                                    <div class="col-sm-3 col-md-3">
+                                        <!-- Card -->
+                                        <article class="card card-inverse animated fadeInRight">
+                                            <img class="img-responsive" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/8SALDQRRZX.jpg" alt="White sand" />
+                                            <div class="card-img-overlay">
+                                                <p class="card-text">Disney</p>
+                                            </div>
+                                        </article><!-- .end Card -->
+                                    </div>
+                                    <div class="col-sm-3 col-md-3">
+                                        <!-- Card -->
+                                        <article class="card card-inverse animated fadeInRight">
+                                            <img class="img-responsive" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/8SALDQRRZX.jpg" alt="White sand" />
+                                            <div class="card-img-overlay">
+                                                <p class="card-text">Logos</p>
+                                            </div>
+                                        </article><!-- .end Card -->
+                                    </div>
+                                    <div class="col-sm-3 col-md-3">
+                                        <!-- Card -->
+                                        <article class="card card-inverse animated fadeInRight">
+                                            <img class="img-responsive" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/8SALDQRRZX.jpg" alt="White sand" />
+                                            <div class="card-img-overlay">
+                                                <p class="card-text">Spell Bee</p>
+                                            </div>
+                                        </article><!-- .end Card -->
+                                    </div>
+                                </div><!-- .end Second row -->
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="played" role="tabpanel" aria-labelledby="play-tab">
+                            Played
+                        </div>
+                        <div class="tab-pane fade" id="details" role="tabpanel" aria-labelledby="stat-tab">
+                            stats
+                        </div>
+
+                    </div>
+
+                </div>
+
+                    </div>
+                </div>
+                <div class="col-sm-3 col-md-3 col-lg-4" >
+                    <div class="card" >
+                        <div class="card-header">
+                            <ul class="nav nav-tabs card-header-tabs" id="myTab1" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link" role="tab" id="reccat-tab" data-toggle="tab" href="#reccategories" aria-controls="cat" aria-selected="false"
+                                       style="">Rec Categories</a>
+                                </li>
+                            </ul>
+                </div>
+                        <div class="card-body">
+                            <div class="tab-content" id="myTabContent">
+                                <div class="tab-pane fade show" id="reccategories" style="padding-top: 2%" role="tabpanel" aria-labelledby="reccat-tab">
+                                    <div class="container-fluid">
+                                    <div class="row" style="padding: 2% 2% 2% 2%">
+                                        <div class="col-sm-3 col-md-6">
+                                            <!-- Card -->
+                                            <article class="card card-inverse animated fadeInRight">
+                                                <img class="img-responsive" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/8SALDQRRZX.jpg" alt="White sand" />
+                                                <div class="card-img-overlay">
+                                                    <p class="card-text">Story </p>
+                                                </div>
+                                            </article><!-- .end Card -->
+                                        </div>
+                                        <div class="col-sm-3 col-md-6">
+                                            <!-- Card -->
+                                            <article class="card card-inverse animated fadeInRight">
+                                                <img class="img-responsive" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/8SALDQRRZX.jpg" alt="White sand" />
+                                                <div class="card-img-overlay">
+                                                    <p class="card-text">Four </p>
+                                                </div>
+                                            </article><!-- .end Card -->
+                                        </div>
+                                        <div class="col-sm-3 col-md-6">
+                                                <!-- Card -->
+                                                <article class="card card-inverse animated fadeInLeft">
+                                                    <img class="img-responsive" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/1U2EGZ07GU.jpg" alt="Deer in nature" />
+                                                    <div class="card-img-overlay">
+                                                        <p class="card-text"> play</p>
+                                                    </div>
+                                                </article><!-- .end Card -->
+                                            </div>
+                                            <div class="col-sm-3 col-md-6">
+                                                <!-- Card -->
+                                                <article class="card card-inverse animated fadeInDown">
+                                                    <img class="img-responsive" src="https://snap-photos.s3.amazonaws.com/img-thumbs/960w/SYC0YBA37V.jpg" alt="Leaf on the street" />
+                                                    <div class="card-img-overlay">
+                                                        <p class="card-text"> She die...</p>
+                                                    </div>
+                                                </article><!-- .end Card -->
+                                            </div>
+
+
+                                     </div>
+                                     </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        </div>
+                </div>
+            </div>
+
+        </main>
+        <!-- partial:partials/_footer.html -->
+        <footer>
+            <div class="mdc-layout-grid">
+                <div class="mdc-layout-grid__inner">
+                    <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6">
+                        <span class="text-muted">Copyright © 2018 <a class="text-green" href="https://www.bootstrapdash.com/" target="_blank">Bootstrap Dash</a>. All rights reserved.</span>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+        <!-- partial -->
+    </div>
+</div>
+
+
+
+
+</body>
+
+</html>
